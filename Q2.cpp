4d1556d19575f49a32ea9d7f4c2eeb6f1736399f
sarthaksharma191 <34069116+sarthaksharma191@users.noreply.github.com>
2018-10-16 12:45:02 +0530

    Changed Variable Labels

--- Q2.cpp
+++ Q2.cpp
@@ -5,15 +5,15 @@ int main()
 
 	int arr[10], i, num, n, c=0, pos;
 	cout<<"Enter the array size : ";
-	cin>>n;
+	cin>>size;
 	cout<<"Enter Array Elements : ";
-	for(i=0; i<n; i++)
+	for(i=0; i<size; i++)
 	{
 		cin>>arr[i];
 	}
 	cout<<"Enter the number to search : ";
 	cin>>num;
-	for(i=0; i<n; i++)
+	for(i=0; i<size; i++)
 	{
 		if(arr[i]==num)
 		{
@@ -32,4 +32,4 @@ int main()
 	}
 
 }
-
+//Changed Size
